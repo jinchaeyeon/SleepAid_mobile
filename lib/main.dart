4927ba044cf298b64c7c4e3c5ea08a7619df2725
Pitzza <appulele@gmail.com>
2022-08-08 20:04:09 +0900

    안드로이드 푸시 세팅 설정(IOS 프로필 제외)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:provider/provider.dart';
 import 'package:sleepaid/app_routes.dart';
@@ -9,13 +13,11 @@ import 'package:sleepaid/page/splash_page.dart';
 import 'package:sleepaid/provider/bluetooth_provider.dart';
 import 'package:sleepaid/provider/data_provider.dart';
 import 'package:sleepaid/provider/main_provider.dart';
-import 'data/firebase/firebase_data.dart';
+import 'data/firebase/firebase_option.dart';
+import 'data/firebase/message.dart';
 import 'data/local/app_dao.dart';
 import 'util/app_config.dart';
 import 'provider/auth_provider.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// import 'package:firebase_core/firebase_core.dart';
 
 /**
  * 플러터 기본 실행 함수
@@ -31,15 +33,15 @@ void main() async {
   await mainInit();
 }
 
-// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   // If you're going to use other Firebase services in the background, such as Firestore,
-//   // make sure you call `initializeApp` before using other Firebase services.
-//   await Firebase.initializeApp();
-//   print('Handling a background message ${message.messageId}');
-// }
-//
-// late AndroidNotificationChannel channel;
-// late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
+late AndroidNotificationChannel channel;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
 
 /**
@@ -47,16 +49,45 @@ void main() async {
  */
 Future<void> mainInit() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await AppDAO.init();
-  // await Firebase.initializeApp(
-  //   options: const FirebaseOptions(
-  //     apiKey: 'AIzaSyAFDeW4erT-KBCQSnAeV-z6gYvjXdycxys',
-  //     appId: '1:492902760774:android:90756f26996bc95b4fe547',
-  //     messagingSenderId: '492902760774',
-  //     projectId: 'sleepaid-1eae8',
-  //   ),
-  // );
-  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("fcm Token : ${fcmToken}");
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+    'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance
+      .setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  FirebaseMessaging.instance.onTokenRefresh
+      .listen((fcmToken) {
+    /// todo 업데이트 코드 입력 API 추가 처리
+  })
+      .onError((err) {
+  });
 
   //화면 회전 막는 기능
   if(!AppDAO.debugData.cancelBlockRotationDevice){
@@ -94,47 +125,46 @@ class _SleepAIDApp extends State<SleepAIDApp> {
   @override
   void initState() {
     super.initState();
-    // FirebaseMessaging.instance
-    //     .getInitialMessage()
-    //     .then((RemoteMessage? message) {
-    //   if (message != null) {
-    //     Navigator.pushNamed(
-    //       context,
-    //       '/message',
-    //       arguments: MessageArguments(message, true),
-    //     );
-    //   }
-    // });
-    //
-    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    //   RemoteNotification? notification = message.notification;
-    //   AndroidNotification? android = message.notification?.android;
-    //   if (notification != null && android != null && !kIsWeb) {
-    //     flutterLocalNotificationsPlugin.show(
-    //       notification.hashCode,
-    //       notification.title,
-    //       notification.body,
-    //       NotificationDetails(
-    //         android: AndroidNotificationDetails(
-    //           channel.id,
-    //           channel.name,
-    //           // TODO add a proper drawable resource to android, for now using
-    //           //      one that already exists in example app.
-    //           icon: 'launch_background',
-    //         ),
-    //       ),
-    //     );
-    //   }
-    // });
-    //
-    // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-    //   print('A new onMessageOpenedApp event was published!');
-    //   Navigator.pushNamed(
-    //     context,
-    //     '/message',
-    //     arguments: MessageArguments(message, true),
-    //   );
-    // });
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? message) {
+      if (message != null) {
+        Navigator.pushNamed(
+          context,
+          '/message',
+          arguments: MessageArguments(message, true),
+        );
+      }
+    });
+
+    ///todo 로컬 디비에 저장된 값으로 이부분 변경
+    FirebaseMessaging.instance.subscribeToTopic("all");
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null && !kIsWeb) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              // TODO add a proper drawable resource to android, for now using
+              //      one that already exists in example app.
+              icon: 'launch_background',
+            ),
+          ),
+        );
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      Navigator.pushNamed(context, Routes.splash);
+    });
   }
 
   void changeTheme() {
--- lib/main_debug.dart
+++ lib/main_debug.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:provider/provider.dart';
 import 'package:sleepaid/app_routes.dart';
@@ -9,11 +13,11 @@ import 'package:sleepaid/page/splash_page.dart';
 import 'package:sleepaid/provider/bluetooth_provider.dart';
 import 'package:sleepaid/provider/data_provider.dart';
 import 'package:sleepaid/provider/main_provider.dart';
-import 'package:sleepaid/util/app_themes.dart';
+import 'data/firebase/firebase_option.dart';
+import 'data/firebase/message.dart';
 import 'data/local/app_dao.dart';
 import 'util/app_config.dart';
 import 'provider/auth_provider.dart';
-
 
 /**
  * 플러터 기본 실행 함수
@@ -24,12 +28,62 @@ void main() async {
   await mainInit();
 }
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
+late AndroidNotificationChannel channel;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+
 /**
  * 기본 실행 시 호출 되어야하는 기본 값 함수
  */
 Future<void> mainInit() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await AppDAO.init();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("fcm Token : ${fcmToken}");
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+    'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance
+      .setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  FirebaseMessaging.instance.onTokenRefresh
+      .listen((fcmToken) {
+    /// todo 업데이트 코드 입력 API 추가 처리
+  })
+      .onError((err) {
+  });
+
   //화면 회전 막는 기능
   if(!AppDAO.debugData.cancelBlockRotationDevice){
     SystemChrome.setPreferredOrientations(
@@ -63,6 +117,51 @@ class _SleepAIDApp extends State<SleepAIDApp> {
   //페이지를 Router를 통하여 관리하기 위한 기본 RouteObserver
   final routeObserver = RouteObserver<PageRoute>();
 
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? message) {
+      if (message != null) {
+        Navigator.pushNamed(
+          context,
+          '/message',
+          arguments: MessageArguments(message, true),
+        );
+      }
+    });
+
+    ///todo 로컬 디비에 저장된 값으로 이부분 변경
+    FirebaseMessaging.instance.subscribeToTopic("all");
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null && !kIsWeb) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              // TODO add a proper drawable resource to android, for now using
+              //      one that already exists in example app.
+              icon: 'launch_background',
+            ),
+          ),
+        );
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      Navigator.pushNamed(context, Routes.splash);
+    });
+  }
+
   void changeTheme() {
     checkDarkMode();
     setState(() {});
@@ -93,38 +192,6 @@ class _SleepAIDApp extends State<SleepAIDApp> {
         return Routes.generateRoute(settings,context);
       },
     );
-    // return AppDAO.completeInit
-    //     ?MaterialApp(
-    //     // 디버그모드 알림 배너 숨김
-    //     debugShowCheckedModeBanner: false,
-    //     //기본 테마
-    //     theme: AppDAO.theme,
-    //     navigatorObservers: [
-    //       routeObserver
-    //     ],
-    //     routes: {
-    //       '/': (_) => const SplashPage(),
-    //     },
-    //     initialRoute: SplashPage.ROUTE,
-    //     // home: Container()
-    //   )
-    //   :MaterialApp(
-    //   // 디버그모드 알림 배너 숨김
-    //   debugShowCheckedModeBanner: false,
-    //   //기본 테마
-    //   theme: AppDAO.theme,
-    //   initialRoute: SplashPage.ROUTE,
-    //   // color: Colors.transparent,
-    //   navigatorObservers: [
-    //     routeObserver
-    //   ],
-    //   routes: {
-    //     '/': (_) => const SplashPage(),
-    //   },
-    //   onGenerateRoute: (RouteSettings settings){
-    //     return Routes.generateRoute(settings,context);
-    //   },
-    // );
   }
 
   Future<void> checkMicrophoneStatus(BuildContext context) async {
